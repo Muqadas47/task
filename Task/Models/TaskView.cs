c3e59a50879713a3458e8e6c0252225cae86098d
Muqadas47 <muqadas.shahzadi35@gmail.com>
2024-09-11 12:50:14 +0500

    First

---
+++ Task/Models/TaskView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task.Models
+{
+    public class TaskView
+    {
+        
+    public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string AssignedTo { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
